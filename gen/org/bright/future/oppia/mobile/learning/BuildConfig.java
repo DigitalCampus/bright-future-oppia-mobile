9ebd75fbd7921d21eb8478794c350c2e99af0ec7
challa913 <ganesh.banoth@gmail.com>
2015-05-29 12:49:20 +0530

    deleting auto generated files bin/ gen/

--- gen/org/bright/future/oppia/mobile/learning/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.bright.future.oppia.mobile.learning;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
